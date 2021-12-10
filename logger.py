b9349499307f20fef412b83148de44acb90ddc67
Pao <pzsigua@gmail.com>
2021-12-10 12:36:54 -0800

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,48 +1,33 @@
+from datetime import datetime
+
 class Logger(object):
     def __init__(self, file_name):
-        # TODO:  Finish this initialization method. The file_name passed should be the
-        # full file name of the file that the logs will be written to.
-        pass
+        self.file_name = file_name
 
-    # The methods below are just suggestions. You can rearrange these or 
-    # rewrite them to better suit your code style. 
-    # What is important is that you log the following information from the simulation:
-    # Meta data: This shows the starting situtation including:
-    #   population, initial infected, the virus, and the initial vaccinated.
-    # Log interactions. At each step there will be a number of interaction
-    # You should log:
-    #   The number of interactions, the number of new infections that occured
-    # You should log the results of each step. This should inlcude: 
-    #   The population size, the number of living, the number of dead, and the number 
-    #   of vaccinated people at that step. 
-    # When the simulation concludes you should log the results of the simulation. 
-    # This should include: 
-    #   The population size, the number of living, the number of dead, the number 
-    #   of vaccinated, and the number of steps to reach the end of the simulation. 
+    def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate, basic_repro_num):
+        f = open(self.file_name, 'w')
 
-    def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
-                       basic_repro_num):
-        # TODO: Finish this method. This line of metadata should be tab-delimited
-        # it should create the text file that we will store all logs in.
-        # TIP: Use 'w' mode when you open the file. For all other methods, use
-        # the 'a' mode to append a new log to the end, since 'w' overwrites the file.
-        # NOTE: Make sure to end every line with a '/n' character to ensure that each
-        # event logged ends up on a separate line!
-        pass
+        metadata = ''
 
-    def log_interactions(self, step_number, number_of_interactions, number_of_new_infections):
-        # TODO: Finish this method. Think about how the booleans passed (or not passed)
-        # represent all the possible edge cases. Use the values passed along with each person,
-        # along with whether they are sick or vaccinated when they interact to determine
-        # exactly what happened in the interaction and create a String, and write to your logfile.
-        pass
+        date_time = datetime.now()
+        header = f'Herd Immunity Simulation: {virus_name}\n'
+        data = f'Starting Population: {pop_size} | Vaccinated Percentage: {vacc_percentage} | Virus: {virus_name} | Mortality Rate: {mortality_rate} | Reproduction Rate: {basic_repro_num}\n\n'
 
-    def log_infection_survival(self, step_number, population_count, number_of_new_fatalities):
-        # TODO: Finish this method. If the person survives, did_die_from_infection
-        # should be False.  Otherwise, did_die_from_infection should be True.
-        # Append the results of the infection to the logfile
-        pass
+        metadata += (date_time.strftime("%c") + '\n')
+        metadata += header
+        metadata += data
 
-    def log_time_step(self, time_step_number):
-        # 
-        pass
+        f.write(metadata)
+        f.close()
+
+    def log_results(self, step_number, number_of_new_infections, vaccinated, dead):
+        f = open(self.file_name, 'a')
+        results = f'Step: {step_number}\n New Infections: {number_of_new_infections} | Total Vaccinated: {vaccinated} | Total Dead: {dead}\n'
+        f.write(results)
+        f.close()
+
+    def log_end(self, population_count, pop_dead, pop_alive):
+        f = open(self.file_name, 'a')
+        result = f'\n*Simulation Has Ended*\n Results:\n Starting Population: {population_count} | Amount Dead: {pop_dead} | Amount Vaccinated : {pop_alive}\n'
+        f.write(result)
+        f.close()
