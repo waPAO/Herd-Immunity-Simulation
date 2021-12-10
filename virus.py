9812d4ad09472e06eb9c361da83aed1b8451cc2f
Pao <pzsigua@gmail.com>
2021-12-09 21:36:46 -0800

    Create virus.py

---
+++ virus.py
@@ -0,0 +1,17 @@
+class Virus(object):
+    # Properties and attributes of the virus used in Simulation.
+    def __init__(self, name, repro_rate, mortality_rate):
+        # Define the attributes of your your virus
+        self.name = name
+        # TODO Define the other attributes of Virus
+        pass
+
+
+# Test this class
+if __name__ == "__main__":
+    # Test your virus class by making an instance and confirming 
+    # it has the attributes you defined
+    virus = Virus("HIV", 0.8, 0.3)
+    assert virus.name == "HIV"
+    assert virus.repro_rate == 0.8
+    assert virus.mortality_rate == 0.3
