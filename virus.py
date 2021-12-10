1a4739a6c71b58d1d65943cdc7e7742e6c2800b5
Pao <pzsigua@gmail.com>
2021-12-10 09:16:28 -0800

    Update virus.py

--- virus.py
+++ virus.py
@@ -1,10 +1,8 @@
 class Virus(object):
-    # Properties and attributes of the virus used in Simulation.
     def __init__(self, name, repro_rate, mortality_rate):
-        # Define the attributes of your your virus
         self.name = name
-        # TODO Define the other attributes of Virus
-        pass
+        self.repro_rate = repro_rate
+        self.mortality_rate = mortality_rate
 
 
 # Test this class
